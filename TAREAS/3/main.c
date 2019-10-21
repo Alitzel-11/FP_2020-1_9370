3d20a281581817aceb84b175161bfbb91ba8473d
ALITZEL GTZ <alitzel.gutierrez31@gmail.com>
2019-10-21 10:54:14 -0500

    TAREA3

--- TAREAS/3/main.c
+++ TAREAS/3/main.c
@@ -6,16 +6,14 @@ int main(int argc, char *argu[]){
 	n1=atof(argu[1]);
 	n2=atof(argu[2]);
 	s=n1+n2;
-    printf("suma:\n%.2f\n",s);
+    printf("%.2f\n",s);
     r=n1-n2;
-	printf("resta:\n%.2f\n", r);
+	printf("%.2f\n", r);
 	d=n1/n2;
-	printf("division:\n%.2f\n", d);
+	printf("%.2f\n", d);
 	m=n1*n2;
-	printf("producto:\n%.2f\n",m);
+	printf("%.2f\n",m);
 
 	return 0;
 
  }
-
- 
