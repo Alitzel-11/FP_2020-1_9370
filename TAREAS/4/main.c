2c5b3fe8dd4b7e2e4850d936856d54d801142bfa
Alitzel Gutiérrez <54130609+Alitzel-11@users.noreply.github.com>
2019-11-13 01:59:16 -0600

    Corregí tarea 4

--- TAREAS/4/main.c
+++ TAREAS/4/main.c
@@ -1,20 +1,20 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h>Biblioteca estandar en C
+#include <stdlib.h>Libreria
 
-int main(int argc,char *argu[])
+int main(int argc,char *argu[])//Añadimos lista 
 {
-	int lista[argc],suma=0;
-    float promedio;
-    for (int i=1;i<argc;i++){
-    	lista[i]=atoi(argu[i]);	
-    }
-    for (int i=1;i<argc;i++){
-    	suma=suma+lista[i];
-    }
-    promedio=((float)suma/(argc-1));
-    printf("%f\n",promedio);
-    return 0;
-    }
-    
-
- 
+	double lista[argc],suma=0;//Declaramos lista
+	double promedio;
+for (int i=1;i<argc;i++)//Ciclo for
+{
+	lista[i]=atof(argu[i]);//Mostramos lista y guardamos elementos convertidos a float
+}
+for (int i=1;i<argc;i++)//Ciclo for
+{
+	suma=suma+lista[i];//Sumatoria de la suma con la lista
+}
+promedio=((double)suma/(argc-1));//Resultado
+printf("%.2f\n",promedio);//Se imprime
+	
+return 0;
+}
