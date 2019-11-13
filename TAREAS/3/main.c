36f661be473ebabaaa50de2fe44081d7ed4254e9
Alitzel Gutiérrez <54130609+Alitzel-11@users.noreply.github.com>
2019-11-13 00:27:43 -0600

    main.c
    Corregí tarea 3

--- TAREAS/3/main.c
+++ TAREAS/3/main.c
@@ -1,19 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h>//Biblioteca standard de C
+#include <stdlib.h>//Libreria que contiene funciones como atoi y atof
 
-int main(int argc, char *argu[]){
-	float n1,n2,s,r,m,d;
-	n1=atof(argu[1]);
-	n2=atof(argu[2]);
-	s=n1+n2;
-    printf("%.2f\n",s);
-    r=n1-n2;
-	printf("%.2f\n", r);
-	d=n1/n2;
-	printf("%.2f\n", d);
-	m=n1*n2;
-	printf("%.2f\n",m);
-
-	return 0;
-
- }
+int main (int argc, char *argu[])//Añadí lista fuera del programa
+{
+float numero,operador,suma,resta,multip,division;//Declare variables
+	numero=atof(argu[1]);//Convierte argumento o variable float
+	operador=atof(argu[2]);//Convierte argumento o variable float
+suma=numero+operador;//Declaré la operación suma
+printf("%.2f\n",suma);//Imprimí el resultado de la suma
+	resta=numero-operador;//Declaré la operación resta
+	printf("%.2f\n", resta);//Imprimí el resultado de la resta
+multip=numero*operador;//Declaré la operación multiplicación
+printf("%.2f\n",multip);//Imprimí el resultado de la multiplicación
+	division=numero/operador;//Declaré la operación división
+	printf("%.2f\n", division);//Imprimí el resultado de la división
+	
+	
+	
+return 0;//Cerramos el programa
+}
