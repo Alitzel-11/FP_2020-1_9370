0e74226b47197cc0a789488ceb7669bd8b3d71e5
Alitzel Gutiérrez <54130609+Alitzel-11@users.noreply.github.com>
2019-11-13 02:53:10 -0600

    Corregí tarea 7

--- TAREAS/7/main.c
+++ TAREAS/7/main.c
@@ -1,16 +1,20 @@
-#include <stdio.h>
-#include <stdlib.h>
-int main(int argc,char*argu[]){
-float n1,n2,resultado;
-    n1= atof(argu[1]);
-    n2= atof(argu[2]);
-if(n1==0){
-    resultado=(n2*1.8)+32;
-    printf("%.2f",resultado);
+#include <stdio.h>//Biblioteca standard de C
+#include <stdlib.h>//Libreria
+
+int main(int argc,char* argv[]){//Funcion principal y argumentos; argv y argc son la forma en la que los argumentos en linea se pasan a main
+
+    float n1,n2,resultado=1;//resultado que se va a imprimir
+    
+  //atof convierte una cadena de caracteres a float
+n1= atof(argu[1]);//Argumento 1 representara a n1
+n2= atof(argu[2]);//Argumento 2 representara a n2
+    for(int a=1;a<=n2;a++){//Variable de control a=1, bucle de continuacion a<= n2 y el incremento de a++
+
+        resultado=resultado*n1;//El resultado
 }
-if(n1==1){
-    resultado=(n2-32)/1.8;
-     printf("%.2f",resultado);
-}
-return 0;
+    
+printf("%.2f",resultado);//Imprimí el resultado
+    
+return 0;//La ejecucion abandona la funcion
+    
 }
